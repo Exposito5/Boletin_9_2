9e019e0dbf29cc5b3fb89cf47f56cbff6fc941d3
Familia <Familia@Arturo.home>
2016-12-15 23:02:52 +0100

    Commit 9.2

--- src/boletin_9_2/SumProd.java
+++ src/boletin_9_2/SumProd.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boletin_9_2;
 
-/**
- *
- * @author Familia
- */
 public class SumProd {
 
     public void calcular() {
